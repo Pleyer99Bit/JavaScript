da0478693d1f37760e7bad66840e95ed765b3d62
Pleyer99Bit <zaffa2393@gmail.com>
2022-03-03 08:32:46 +0100

    .

--- src/JavaScript.js
+++ src/JavaScript.js
@@ -1,11 +1,3 @@
 function Invio(){
-    var nome = document.getElementById("nome").value;
-    var data = document.getElementById("data").value;
-    var eta = document.getElementById("eta").value;
-    var email = document.getElementById("email").value;
-    alert(nome + " " + eta + " " + email + " " + data);
-    document.getElementById("nome").setvalue = none;
-    document.getElementById("data").value = none;
-    document.getElementById("eta").value = none;
-    document.getElementById("email").value = none;
+    
 }
